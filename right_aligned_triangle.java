56dad72d73b43cb9cbca99b1a675475093cf5d2c
Pratyush Kumar <101619709+Code-with-pratyush@users.noreply.github.com>
2023-07-30 12:02:52 +0530

    Add files via upload

---
+++ right_aligned_triangle.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class right_aligned_triangle{
+    public static void main(String args[]){
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the value of n");
+        int n=sc.nextInt();
+        for(int i=0; i<=n;i++){
+            for(int j = 0 ; j <= n -i-1;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int k=0;k<i;k++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
